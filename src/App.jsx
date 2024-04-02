5edd722e89c7d0d2babf5d66e838f7e98c490eeb
PiyushGaurav <piyushgaurav07@gmail.com>
2024-04-02 17:39:19 +0530

    initial commit with taiwind setup'

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function App() {
+	return (
+		<div class="space-y-5">
+			<h1 className="text-3xl font-bold underline">Hello world!</h1>
+		</div>
+	);
+}
